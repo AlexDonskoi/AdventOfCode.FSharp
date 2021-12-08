182492cc781d0d23b01638c05a14cb4963c4d7ae
Alex Donskoy <Alexander.Donskoy@dataart.com>
2021-12-08 22:48:10 +0300

    update 2021/7 to faster algorithm

--- AdventOfCode.Cases/Y2021/Day7.fs
+++ AdventOfCode.Cases/Y2021/Day7.fs
@@ -4,21 +4,53 @@ open AdventOfCode.Cases.Infrastructure
 
 let costB distance = (distance * distance + distance) /2L
 let costA distance = distance
-let costAll cost pos = List.fold (fun acc cur -> pos - cur |> abs |> cost |> (+) acc) 0L
+let costAll cost pos = (-) <| pos >> abs >> cost |> List.sumBy
 
-let rec next cost compare src =
-    let decrement = (+) -1L
-    let state = List.map decrement src
-    let stateCost = List.sumBy (abs >> cost) state
-    if compare > 0L && stateCost > compare then compare
-        else next cost stateCost state
+let tupleMap3 f (x,y,z) = f x, f y, f z
+
+(*
+    In assumption cost function have single local minimum. Count in 5 point and decide which half contains minimum
+    use power of 2 so can continue same way with half
+    start  mid      finish
+    |       |       |
+    |...|...|...|...|
+       left   right
+
+*)
+let rec calculate cost step points distances =
+    let start,mid,finish =
+        match points with
+        | None -> (0L, 1L, 2L) |> tupleMap3 ((*) step >> cost >> (|>) distances)
+        | Some b -> b
+
+    let midStep = step / 2L
+    let left = cost midStep distances
+    let right = cost (3L*midStep) distances
+    let calculate = Some >> calculate cost midStep
+    match midStep with
+    | 1L -> List.min [start; left; mid; right; finish;]
+    | _ when left < mid -> calculate (start, left, mid) distances
+    | _ when right < mid ->
+        let distances = List.map (fun v -> v - step) distances // adjust distances related to new starting point
+        calculate (mid, right, finish) distances
+    | _ ->
+        let distances = List.map (fun v -> v - midStep) distances // adjust distances related to new starting point
+        calculate (left, mid, right) distances
+
+let rec step acc src =
+    match src with
+    | 1L -> acc
+    | _ -> step <| (acc * 2L) <| (src / 2L)
+
 
 [<Puzzle(2021, 7)>]
 let puzzle case (input:string) =
-    input
-    |> String.split ","
-    |> Array.map int64
-    |> Array.toList
-    |> match case with
-        | Case.A -> next costA 0L
-        | Case.B -> next costB 0L
+    let cost = costAll <| match case with | Case.A -> costA | Case.B -> costB
+    let src =
+        input
+        |> String.split ","
+        |> Array.map int64
+        |> Array.toList
+
+    let step = src |> List.max |> step 1L
+    calculate cost step None src
--- AdventOfCode.Tests/Y2021/Day7Tests.fs
+++ AdventOfCode.Tests/Y2021/Day7Tests.fs
@@ -6,6 +6,11 @@ open Xunit
 open Swensen.Unquote
 
 [<Fact>]
+let ``Simple case``() =
+    let state = "1,2,3" |> puzzle Case.A
+    test <@ state = 2 @>
+
+[<Fact>]
 let ``Example caseA``() =
     let state = "16,1,2,0,4,2,7,1,2,14" |> puzzle Case.A
     test <@ state = 37 @>
@@ -14,3 +19,13 @@ let ``Example caseA``() =
 let ``Example caseB``() =
     let state = "16,1,2,0,4,2,7,1,2,14" |> puzzle Case.B
     test <@ state = 168 @>
+
+[<Fact>]
+let ``Increase sequence``() =
+    let state = "1,2,3,4,5,6,7,8" |> puzzle Case.A
+    test <@ state = 16 @>
+
+[<Fact>]
+let ``Decrease sequence``() =
+    let state = "8,7,6,5,4,3,2,1" |> puzzle Case.A
+    test <@ state = 16 @>
