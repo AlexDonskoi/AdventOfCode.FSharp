7d86a2dfd1e4ec34c847d850a1da145c73ea02b2
Alex Donskoy <Alexander.Donskoy@dataart.com>
2021-12-26 15:04:55 +0300

    2021/15

--- AdventOfCode.Cases/Y2021/Day15.fs
+++ AdventOfCode.Cases/Y2021/Day15.fs
@@ -1,68 +1,60 @@
 module AdventOfCode.Cases.Y2021.Day15
 
-open System
-open System.IO
 open AdventOfCode.Cases.Infrastructure
 open Microsoft.FSharp.Collections
 open Microsoft.FSharp.Core
 
-open AdventOfCode.Cases.Infrastructure.Parser
 let parse: char[,]->int[,] = Array2D.map (fun v -> int v - 48)
 
-let scale quantity (src:int[,])  =
-    let size = Array2D.length1 src
-    let init src i j =
-        let targetVal = Array2D.get src <| i%size <| j%size
-        (targetVal + (i / size) + (j / size) - 1) % 9 + 1
-    Array2D.init <| size*quantity <| size * quantity <| init src
-
-type State = | Risk of int | Path of int*int
-
-let init src =
-    let result = src |> Array2D.map Risk
-    Array2D.set result 0 0 (Path (0,0))
-    result
-
-let adjacent src i j =
-    let size1, size2 = Array2D.sizes src
+let adjacent size i j =
     [
-        if i = 0 then () else yield i - 1,j
-        if i = size1 then () else yield i + 1,j
-        if j = 0 then () else yield i,j - 1
-        if j = size2 then () else yield i,j + 1
+        if i >= size - 1 then () else yield i + 1,j
+        if i <= 0 then () else yield i - 1,j
+        if j >= size - 1 then () else yield i,j + 1
+        if j <= 0 then () else yield i,j - 1
     ]
-let update src path (i,j) =
-    match Array2D.get src i j with
-    | Path (v, r) -> Path (min v (path+r), r)
-    | Risk r -> Path (path+r, r)
-    |> Array2D.set src i j
-let rec run src =
-    let size, _ = Array2D.sizes src
-    function
-    | 0 -> src.[size, size]
-    | step ->
-        Array2D.iteri (
-            fun i j v ->
-                match v with
-                | Path (v,r) ->
-                    adjacent src i j
-                    |> List.iter (update src v)
-                | _ -> ()
-        ) src
-        run src (step - 1)
+
+let next adjacents source =
+    let listFolder acc cur =
+        if Set.exists (fun (_, p) -> snd cur = p) acc then acc
+                else Set.add cur acc
+    adjacents |> List.fold listFolder source
+
+let risk cur src (i,j) =
+    let normalize v = (v - 1) % 9 + 1
+    let size = src |> Array2D.length1
+    Array2D.get src <| i % size <| j % size
+    |> (+) (i / size)
+    |> normalize
+    |> (+) (j/size)
+    |> normalize
+    |> (+) cur, (i,j)
+
+let rec foldRecursive adjacent target state visited =
+    let w,p = Set.minElement state
+    if p = target then w
+    else
+        let state = Set.remove (w,p) state
+        let state =
+            adjacent (w,p)
+            |> List.filter (fun (_, p) -> Set.contains p visited |> not)
+            |> next
+            <| state
+        let visited = Set.add p visited
+        foldRecursive adjacent target state visited
+
+let run src scale =
+    let size = src |> Array2D.length1 |> (*) scale
+    let adjacent size (w, (i,j)) =
+        adjacent size i j |> List.map (risk w src)
+    foldRecursive <| adjacent size <| (size - 1, size - 1) <| Set [0,(0,0)] <| Set.empty
 
 [<Puzzle(2021, 15)>]
 let puzzle case (input:seq<string>) =
-    let src =
-        input
-        |> array2D
-        |> parse
-        |> match case with
-            | Case.A -> scale 1
-            | Case.B -> scale 5
-
-        |> init
-    let size,_ = Array2D.sizes src
-    match run src (size) with
-    | Path (v,_) -> v
-    | _ -> failwith "path not found"
+    input
+    |> array2D
+    |> parse
+    |> run
+    <| match case with
+        | Case.A -> 1
+        | Case.B -> 5
