39bd441ab4dda687fc7ca8b3f66cddbe0b597841
Aleksandr Donskoi <sanchesdonskoy@gmail.com>
2022-12-07 12:17:45 +0100

    refactor day 7

--- AdventOfCode.Cases/Y2022/Day7.fs
+++ AdventOfCode.Cases/Y2022/Day7.fs
@@ -3,67 +3,71 @@ open System
 open AdventOfCode.Cases.Infrastructure
 open AdventOfCode.Cases.Infrastructure.Parser
 open Microsoft.FSharp.Core
+open Microsoft.Win32.SafeHandles
 
-type Content = | File of string * int | Directory of string
+type Command =
+    | Root
+    | MoveUp
+    | Move of string
+    | Ls
+    | File of string * int
+    | Directory of string
 
+type Path = list<string>
 
+let path = List.rev >> String.concat "/"
 
-let parseContent (src:string)=
+let parseCommand src =
     match String.split " " src with
+    | [| "$"; "cd"; "/" |] -> Root
+    | [| "$"; "cd"; ".." |] -> MoveUp
+    | [| "$"; "cd"; dir |] -> Move dir
+    | [| "$"; "ls" |] -> Ls
     | [| "dir"; dir |] -> Directory dir
     | [| Int size; name |] -> File (name,size)
-    | _ -> failwith "unrecognized format"
+    | _ -> failwith "unrecognized command"
 
-let join = List.fold (fun acc cur -> acc + string cur) ""
+let increaseSize key size = Map.change key (Option.defaultValue 0 >> (+) size >> Some)
+let rec increaseSizeRecursive key size tree =
+    match key with
+    | ["/"] -> "/" |> increaseSize <| size <| tree
+    | _::rest ->
+        let tree = key |> path |> increaseSize <| size <| tree
+        increaseSizeRecursive rest size tree
+    | _ -> tree
 
+let foldTree (path, tree) = function
+    | Root -> ["/"], tree
+    | MoveUp -> List.tail path, tree
+    | Move dir -> dir::path, tree
+    | File (_,size)->
+        path, increaseSizeRecursive path size tree
+    | _ -> path, tree
 
+let run =
+    Seq.map parseCommand
+    >> Seq.fold foldTree (["/"], Map.empty)
+    >> snd
 
 let updateTree (dir:list<string>) size =
     let path = List.rev dir |> List.reduce (+)
     Map.change path (Option.defaultValue 0 >> (+) size >> Some)
 
-let rec updateTreeRec dir size tree =
-    match dir with
-    | "/"::_::rest ->
-        let tree = updateTree dir size tree
-        updateTreeRec rest size tree
-    | _ -> updateTree ["/"] size tree
+let folderA acc _ v = if v <= 100000 then acc + v else acc
 
-
-let treeA (path, tree) = Seq.toList >> function
-    | ['$'; ' '; 'c'; 'd'; '/'] -> ["/"], tree
-    | ['$'; ' '; 'c'; 'd'; ' '; '.' ; '.'] -> List.skip 2 path, tree
-    | '$'::' '::'c'::'d'::dir ->
-        let dir = String.joinSeq "" dir
-        "/"::dir::path, tree
-    | ['$';' ';'l'; 's'] -> path, tree
-    | 'd'::'i'::'r'::dir -> path, tree
-    | file ->
-        match file |> join |> String.split " " with
-        | [| Int size; _ |] ->
-            let tree = updateTreeRec path size tree
-            path, tree
-        | _ -> failwith "no file size"
-    | _ -> failwith "unexpected input"
+let folderB (limit, acc) _ v =
+    let isMatch = (v >= limit) && (v < acc)
+    limit, if isMatch then v else acc
 
 [<Puzzle(2022, 7)>]
 let puzzle case (source:seq<string>) =
-    let map =
-        source
-        |> Seq.fold treeA (["/"], Map.empty)
-        |> snd
+    let map = run source
     match case with
-        | Case.A ->
-            map
-            |> Map.filter (fun k v -> v <= 100000)
-            |>Map.fold (fun acc k v -> acc + v) 0
-        | Case.B ->
-            let space = Map.find "/" map - 40000000
-            map
-            |> Map.toList
-            |> List.map snd
-            |> List.filter ((<=) space)
-            |> List.min
+    | Case.A -> Map.fold folderA 0 map
+    | Case.B ->
+        let space = "/" |> Map.find <| map |> (-) <| 40000000
+        Map.fold folderB (space, 70000000) map
+        |> snd
 
 
 
