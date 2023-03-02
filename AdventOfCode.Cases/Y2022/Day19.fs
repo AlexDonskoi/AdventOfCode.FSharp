043df2f6e8e4e486dd4e8537d942edf6d5c0df20
Aleksandr Donskoi <sanchesdonskoy@gmail.com>
2023-03-02 13:34:52 +0100

    day 19

--- AdventOfCode.Cases/Y2022/Day19.fs
+++ AdventOfCode.Cases/Y2022/Day19.fs
@@ -1,4 +1,5 @@
 module AdventOfCode.Cases.Y2022.Day19
+open System
 open System.Text.RegularExpressions
 open AdventOfCode.Cases.Infrastructure
 open AdventOfCode.Cases.Infrastructure.Parser
@@ -15,49 +16,72 @@ let parse input =
     let getVal = sourceRegex.Match input |> getVal
     ((getVal "ore"), (getVal "clay"), (getVal "obsidianOre"), (getVal "obsidianClay"), (getVal "geodeOre"), (getVal "geodeObsidian"))
 
-let rec next blueprint step strategy state =
-    if step = 0 then Some state
-    else
-    let flag = strategy % 5L
-    let buildGeo = flag = 1L
-    let buildObs = flag = 2L
-    let buildClay = flag = 3L
-    let buildOre = flag = 4L
-    let costOre, costClay, costObsOre, costObsClay, costGeoOre, costGeoObs = blueprint
-    let robots, sources = state
-    let robOre, robClay, robObs, robGeo = robots
-    let ore, clay, obs, geo = sources
+let rec isCovered key1 key2 =
+    let (ore1, clay1, obs1, robOre1, robClay1, robObs1) = key1
+    let (ore2, clay2, obs2, robOre2, robClay2, robObs2) = key2
 
-    if buildGeo && (costGeoOre > ore || costGeoObs > obs) then None
-    else if buildObs && (costObsOre > ore || costObsClay > clay) then None
-    else if buildClay && (costClay > ore) then None
-    else if buildOre && (costOre > ore) then None
+    ore1 >= ore2 && clay1 >= clay2 && obs1 >= obs2 && robOre1 >= robOre2 && robClay1 >= robClay2 && robObs1 >= robObs2
+
+let rec tryAdd key value map =
+    match Map.tryFindKey (fun k v -> isCovered k key && v >= value) map with
+    | Some _ -> map
+    | None ->
+        Map.filter (fun k v -> isCovered key k && value >= v) map
+        |> Map.fold (fun acc k v -> Map.remove k acc) map
+        |>Map.change key (Option.defaultValue value >> max value >> Some)
+
+let rec search (maxOre, maxClay, maxObs, costOre, costClay, costObsOre, costObsClay, costGeoOre, costGeoObs) states remain =
+    let mutable newMap = Map.empty
+
+    if remain = 0 then Map.values states |> Seq.max
     else
 
-        let ore = ore + robOre - (if buildOre then costOre else 0) - (if buildClay then costClay else 0) - (if buildObs then costObsOre else 0)  - (if buildGeo then costGeoOre else 0)
-        let clay = clay + robClay - (if buildObs then costObsClay else 0)
-        let obs = obs + robObs - (if buildGeo then costGeoObs else 0)
-        let geo = geo + robGeo
-        let robOre = robOre + (if buildOre then 1 else 0)
-        let robClay = robClay + (if buildClay then 1 else 0)
-        let robObs = robObs + (if buildObs then 1 else 0)
-        let robGeo = robGeo + (if buildGeo then 1 else 0)
-        let state = (ore, clay, obs, geo), (robOre, robClay, robObs, robGeo)
-        next blueprint (step - 1) (strategy/5L) state
+    for k in Map.keys states do
+        let earn = Map.find k states
+        let (core, cclay, cobs, robOre, robClay, robObs) = k
+        let ore = if robOre >= maxOre then maxOre else (core + robOre)
+        let clay = if robClay >= maxClay then maxClay else (cclay + robClay)
+        let obs = if robObs >= maxObs then maxObs else (cobs + robObs)
+        let needOre = robOre < maxOre //&& (remain*maxOre > (remain - 1)*robOre)
+        let needClay = robClay < maxClay //&& (remain*maxClay > (remain - 1)*robClay)
+        let needObs = robObs < maxObs //&& (remain*maxObs > (remain - 1)*robObs)
+        //let badWay = (remain < 4 && earn = 0) || (remain < 12 && robObs = 0) || (remain < 18 && robClay = 0)
+        //if badWay then states
+        //else
+        newMap<-tryAdd (ore, clay, obs, robOre, robClay, robObs) earn newMap
+        newMap<-
+            if needOre && costOre <= core
+            then
+                let ore = if robOre >= maxOre then maxOre else (core - costOre + robOre)
+                tryAdd (ore, clay, obs, robOre + 1, robClay, robObs) earn newMap
+            else newMap
+        newMap<-
+            if needClay && costClay <= core
+            then
+                let ore = if robClay >= maxClay then maxOre else (core - costClay + robOre)
+                tryAdd (ore, clay, obs, robOre, robClay + 1, robObs) earn newMap
+            else newMap
+        newMap<-
+            if needObs && costObsOre <= core && costObsClay <= cclay
+            then
+                let ore = if robOre >= maxOre then maxOre else (core - costObsOre + robOre)
+                let clay = if robClay >= maxClay then maxClay else (cclay - costObsClay + robClay)
+                tryAdd (ore, clay, obs, robOre, robClay, robObs + 1) earn newMap
+            else newMap
+        newMap<-
+            if costGeoOre <= core && costGeoObs <= cobs
+            then
+                let ore = if robOre >= maxOre then maxOre else (core - costGeoOre + robOre)
+                let obs = if robObs >= maxObs then maxObs else (cobs - costGeoObs + robObs)
 
-let rec searchStrategy blueprint steps strategy total =
-    let state = (1, 0, 0, 0),(0, 0, 0, 0)
-    let current =
-        match next blueprint steps strategy state with
-        | Some state ->
-            let (_, _, _, geo), (_, _, _, robGeo) = state
-            geo + robGeo
-        | None -> 0
-    let total = max current total
-    if strategy > 0L then
-       searchStrategy blueprint steps (strategy - 1L) total
-    else
-        total
+                tryAdd (ore, clay, obs, robOre, robClay, robObs) (earn + remain - 1) newMap
+            else newMap
+    search (maxOre, maxClay, maxObs, costOre, costClay, costObsOre, costObsClay, costGeoOre, costGeoObs) newMap (remain - 1)
+
+let run (costOre, costClay, costObsOre, costObsClay, costGeoOre, costGeoObs) =
+    let maxOre = max costOre costClay |> max costObsOre |> max costGeoOre
+    search (maxOre, costObsClay, costGeoObs, costOre, costClay, costObsOre, costObsClay, costGeoOre, costGeoObs)
+
 
 [<Puzzle(2022, 19)>]
 let puzzle case (source:seq<string>) =
@@ -66,15 +90,12 @@ let puzzle case (source:seq<string>) =
     let cycles, filter, reduce =
         match case with
         | Case.A ->
-            13, id, List.mapi (fun i v -> (i + 1) * v) >>  List.reduce (+)
+            24, id, List.mapi (fun i v -> (i + 1) * v) >>  List.reduce (+)
         | Case.B ->
-            let take = max 3 <| List.length blueprints
+            let take = min 3 <| List.length blueprints
             32, List.take take, List.fold (*) 1
-    let cycles = cycles - 1
-    let max = pown 5L cycles - 1L
 
-    let maxCount blueprint = searchStrategy blueprint cycles max 0
-    let blueprints = filter blueprints
-    blueprints
-    |> List.map maxCount
+    let initState = Map.empty |> Map.add (0, 0, 0, 1, 0, 0) 0
+    filter blueprints
+    |> List.map (fun b -> run b initState cycles)
     |> reduce
