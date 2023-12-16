10a19bfa2f256310a0fa41eee91129f7ce73a4f2
Aleksandr Donskoi <sanchesdonskoy@gmail.com>
2023-12-16 01:47:56 +0100

    day 12 2023

--- AdventOfCode.Cases/Y2023/Day12.fs
+++ AdventOfCode.Cases/Y2023/Day12.fs
@@ -6,34 +6,27 @@ open AdventOfCode.Cases.Infrastructure
 open AdventOfCode.Cases.Infrastructure.Parser
 open Microsoft.FSharp.Core
     
-let parseSeq = String.split "," >> Array.map int
+let parseSeq = String.split "," >> Array.map int >> Array.toList
 
 let parseRow = String.split " " >> function
-    | [| src; pat |] -> src |> Seq.toArray, parseSeq pat
+    | [| src; pat |] -> src |> Seq.toList, parseSeq pat
     | v -> failwith $"wtf {v}"
-
-
-let chunks = 5
-
-let leftConnected = pown 2 (chunks - 1) |> int64
-let maxConnected = leftConnected * 2L - 1L
 
 type Connections = | No | Left | Right | Both
 
-let groupPattern cur =    
+let groupPattern rightConnected cur =    
     let rec groupPatternRec acc cur =
         let head = List.head acc
         if cur = 0L then
             if head = 0 then List.tail acc else acc
             |> List.rev
-            |> List.toArray
         else
             let rest = List.tail acc
             let acc = if cur &&& 1L = 1L then (head+1)::rest else if head <> 0 then 0::acc else acc
             let cur = cur >>> 1
             groupPatternRec acc cur
-    let left = leftConnected &&& cur = leftConnected
-    let right = cur &&& 1L = 1L
+    let right = rightConnected &&& cur = rightConnected
+    let left = cur &&& 1L = 1L
     let connection =
         match left, right with
         | true, true -> Both
@@ -42,103 +35,116 @@ let groupPattern cur =
         | _, _ -> No
     connection, groupPatternRec [0] cur
     
-let rec isMaskMatchRec mask size cur ind =
-        if ind > size && cur = 0L then true else
-            let notAllowed = if cur &&& 1L = 1L then '.' else '#'
-            let cur = if cur = 0L then 0L else cur >>> 1
-            mask ind <> notAllowed && isMaskMatchRec mask size cur <| ind + 1
-let isMaskMatch mask tgt  =
-    isMaskMatchRec mask chunks tgt 0  
-
-let rec isSubPatternRec target source maxInd connection ind =
-    if ind > maxInd then false else
-        let tgt = Array.get target ind
-        let src = Array.get source ind
-        if ind = maxInd && tgt > src  then connection = Right || connection = Both
-        else if tgt <> src then false
-        else if ind < maxInd then isSubPatternRec target source maxInd connection <| ind + 1
-        else true
-        
-
-let isSubPattern target (connection, source) =
-    let maxInd = Array.length source - 1
-    if maxInd < 0 then true
-    else if Array.length target <= maxInd then false else
-        isSubPatternRec target source maxInd connection 0
-        
-let isPattern target (_, source) =
-    let maxInd = Array.length source - 1
-    if Array.length target <> maxInd + 1 then false else
-        isSubPatternRec target source maxInd No 0          
+let rec isMaskMatch mask cur =
+    match mask with
+    | [] -> cur = 0L
+    | h::rest -> 
+        let notAllowed = if cur &&& 1L = 1L then '.' else '#'
+        let cur = if cur = 0L then 0L else cur >>> 1
+        h <> notAllowed && isMaskMatch rest cur
 
 let optionAdd v = Option.defaultValue 0L >> (+) v >> Some         
  
-let rec optionsRec mask acc cur =
-        if cur < 0L then acc else
-            let acc =
-                if isMaskMatch mask cur |> not then acc else
-                    let pattern = groupPattern cur
-                    Map.change pattern (optionAdd 1L) acc                    
-            optionsRec mask acc <| cur - 1L 
+let rec optionsRec mask pattern leftConnect maxGrp cache cur =
+        if cur < 0L then cache else
+            let cache =
+                if isMaskMatch mask cur |> not then cache else
+                    let pattern = groupPattern leftConnect cur
+                    if pattern |> snd |> List.exists ( (<) maxGrp) then cache else
+                        Map.change pattern (optionAdd 1L) cache                    
+            optionsRec mask pattern leftConnect maxGrp cache <| cur - 1L 
 
 let getMask mask shift ind =
     let size = Array.length mask - 1
     let ind = ind + shift
     if ind > size then '.' else mask.[ind]
+            
+let options mask pattern cache =
+    let max = List.max pattern
+    let size = List.length mask
+    let maxStep = pown 2L size - 1L
+    let leftConnected = pown 2l (size - 1)    
+    optionsRec mask pattern leftConnected max cache maxStep
+    
+let calculateA src =
+    let _, pattern = src
+    let cache = src ||> options <| Map.empty
+    [ No; Left; Right; Both ]
+    |> List.fold (fun acc c -> Map.tryFind (c, pattern) cache |> Option.defaultValue 0L |> (+) acc) 0L
  
-let merge ((connLeft, grpLeft), cntLeft) ((connRight, grpRight), cntRight) =
+let init = Map [(No, []), 1L]
+
+let connect = Map [(No, []), 1L; (Both, [1]), 1L]    
+    
+let merge (connLeft, grpLeft) (connRight, grpRight) =
     let rgpRes =
-        if Array.length grpRight = 0 then Array.copy grpLeft
-        else if Array.length grpLeft = 0  then Array.copy grpRight
+        if List.length grpRight = 0 then grpLeft
+        else if List.length grpLeft = 0  then grpRight
         else
             match connLeft, connRight with
             | Right, Left
             | Right, Both
             | Both, Left
             | Both, Both ->
-                let headRight = Array.head grpRight
-                let grpRight = Array.tail grpRight
-                let size = Array.length grpLeft
-                let grpLeft = Array.copy grpLeft
-                grpLeft.[size-1]<-grpLeft.[size-1] + headRight
-                Array.concat [ grpLeft; grpRight ]
-            |_ -> Array.concat [grpLeft; grpRight]
+                let headRight = List.head grpRight
+                let grpRight = List.tail grpRight
+                let revLeft = grpLeft |> List.rev
+                let lastLeft = List.head revLeft
+                List.concat [ revLeft |> List.tail |> List.rev ; [lastLeft + headRight] ;grpRight ]
+            |_ -> List.concat [grpLeft; grpRight]
     let connection = match connRight with | Right | Both -> Right | _ -> No
-    ((connection, rgpRes), cntLeft*cntRight)
-  
-            
-let rec options mask rootPattern acc shift =
-    let filterMatch root k _ = isSubPattern root k
-    let filterMatch = filterMatch rootPattern
-    let getOptions shift = optionsRec (getMask mask shift) Map.empty <| maxConnected
-    let folder next acc grp cnt =
-        let folder acc k v =
-            let k, v = merge (grp, cnt) (k,v)
-            Map.change k (optionAdd v) acc
-        next
-        |> Map.fold folder acc
+    (connection, rgpRes)   
+
+type TKey = Connections*list<int>
+type TMap = Map<Connections*list<int>, int64>
+
+let rec isSame pattern cur =
+    match pattern, cur with
+    | [],[] -> true
+    | _, [] -> true
+    | _, [_] -> true
+    | [], _ -> false
+    | h1::rest1, h2::rest2 -> h1 = h2 && isSame rest1 rest2
     
-    let next = getOptions shift    
+
+let mergeAdd pattern left acc keyR valR =
+    let max = List.max pattern
     
-    let acc = 
-        acc
-        |> Map.fold (folder next) Map.empty
-    let acc = acc |> Map.filter filterMatch
-    if shift >= Array.length mask then acc else    
-        options mask rootPattern acc <| shift + chunks    
+    let keyL, valL = left
+    let key = merge keyL keyR
+    let tmpPattern = snd key
+    if tmpPattern |> isSame pattern |> not then acc
+    else if List.exists ((<) max) tmpPattern then acc else
+        Map.change key (Option.defaultValue 0L >> (+) (valL * valR) >> Some) acc
+
+let mergeWithMap pattern map acc keyCur valCur = Map.fold (mergeAdd pattern (keyCur,valCur)) acc map     
+ 
+let mergeAll pattern acc left (right:TMap) = Map.fold (mergeWithMap pattern right) acc left
+
     
-let initMap = Map [(No, [||]), 1L]    
+let calculateB src =
+    let _, pattern = src
+    let cache = src ||> options <| Map.empty
+    let parts =
+        [for i in 1..5 do
+            yield i,connect
+            yield i,cache]
+        |> List.removeAt 0
+    
+    let pattern = List.concat [pattern;pattern;pattern;pattern;pattern]
+    let folder acc (i,cur) = mergeAll pattern Map.empty acc cur
+        
+    let res = List.fold folder init parts
+    
+    [ No; Left; Right; Both ]
+    |> List.fold (fun acc c -> Map.tryFind (c, pattern) res |> Option.defaultValue 0L |> (+) acc) 0L
     
 [<Puzzle(2023, 12)>]
 let puzzle case (source:seq<string>) =
     let source = source |> Seq.map parseRow
-              
-    match case with
-    | Case.A ->
-        let getMatchOptions cur =
-            let mask, rootPattern = cur
-            options mask rootPattern initMap 0
-            |> Map.filter (fun k v -> isPattern rootPattern k)
-        let options = source |> Seq.fold (fun acc cur -> getMatchOptions cur) Map.empty
-        0L
-    | Case.B -> 0L
+    let calculate =           
+        match case with
+        | Case.A -> calculateA
+        | Case.B -> calculateB
+    source
+    |> Seq.fold (fun acc cur -> calculate cur |> (+) acc ) 0L
--- AdventOfCode.Tests/Y2023/Day12Tests.fs
+++ AdventOfCode.Tests/Y2023/Day12Tests.fs
@@ -5,77 +5,38 @@ open AdventOfCode.Cases.Y2023.Day12
 open AutoFixture.Xunit2
 open Xunit
 open Swensen.Unquote
-
-
-
-[<Fact>]
-let ``isSubPattern test 1``() =
-
-    let actual = isSubPattern [|1; 1|] (No, [|1|])
-    test <@ actual @>
-    
-[<Fact>]
-let ``isSubPattern test 2``() =
-
-    let actual = isSubPattern [|1; 1|] (No, [|1; 1|])
-    test <@ actual @>    
- 
-[<Fact>]
-let ``isSubPattern test 3``() =
-
-    let actual = isSubPattern [|1; 1|] (No, [|1; 1; 1|])
-    test <@ actual |> not @>  
-       
-[<Fact>]
-let ``isSubPattern test 4``() =
-
-    let actual = isSubPattern [|1; 2|] (No, [|1; 1|])
-    test <@ actual |> not @>
-    
-[<Fact>]
-let ``isSubPattern test 5``() =
-
-    let actual = isSubPattern [|1; 2|] (Right, [|1; 1|])
-    test <@ actual @>
     
 [<Fact>]
 let ``merge test 1``() =
 
-    let actual = merge ((No, [|1; 1|]), 2) ((No, [|1; 1|]), 3)
-    test <@ actual = ((No, [|1; 1; 1; 1|]), 6) @>
+    let actual = merge (No, [1; 1]) (No, [1; 1])
+    test <@ actual = (No, [1; 1; 1; 1]) @>
     
 [<Fact>]
 let ``merge test 2``() =
 
-    let actual = merge ((No, [|1; 1|]), 2) ((Left, [|1; 1|]), 3)
-    test <@ actual = ((No, [|1; 1; 1; 1|]), 6) @>
+    let actual = merge (No, [1; 1]) (Left, [1; 1])
+    test <@ actual = (No, [1; 1; 1; 1]) @>
     
 [<Fact>]
 let ``merge test 3``() =
 
-    let actual = merge ((No, [|1; 1|]), 2) ((Both, [|1;1|]), 3)
-    test <@ actual = ((Right, [|1; 1; 1; 1|]), 6) @>
+    let actual = merge (No, [1; 1]) (Both, [1;1])
+    test <@ actual = (Right, [1; 1; 1; 1]) @>
     
     
 [<Fact>]
 let ``merge test 4``() =
 
-    let actual = merge ((Right, [|1;1|]), 2) ((Both, [|1;1|]), 3)
-    test <@ actual = ((Right, [|1; 2; 1|]), 6) @>
+    let actual = merge (Right, [1;1]) (Both, [1;1])
+    test <@ actual = (Right, [1; 2; 1]) @>
     
 [<Fact>]
 let ``merge test 5``() =
 
-    let actual = merge ((Right, [|1;1|]), 2) ((Left, [|1;1|]), 3)
-    test <@ actual = ((No, [|1; 2; 1|]), 6) @>
-    
-[<Fact>]
-let ``Get options 1``() =
-
-    let actual = options [| '#'; '#'; '#'|] [|3|] initMap 0
-    let expected = Map [(No, [|3|]), 1L] 
-    test <@ actual = expected @> 
-                
+    let actual = merge (Right, [1;1]) (Left, [1;1])
+    test <@ actual = (No, [1; 2; 1]) @>
+                    
 [<Theory>]
 [<InlineData("??.# 1,1", 2L)>]
 [<InlineData("???.### 1,1,3", 1L)>]
@@ -88,4 +49,18 @@ let ``Puzzle example 1``(src, expected) =
 
     let src =  Seq.singleton src 
     let actual = puzzle Case.A src
-    test <@ actual = expected @>        
+    test <@ actual = expected @>
+    
+[<Theory>]
+[<InlineData("? 1", 1L)>]
+[<InlineData("???.### 1,1,3", 1L)>]
+[<InlineData(".??..??...?##. 1,1,3", 16384L)>]
+[<InlineData("?#?#?#?#?#?#?#? 1,3,1,6", 1L)>]
+[<InlineData("????.#...#... 4,1,1", 16L)>]
+[<InlineData("????.######..#####. 1,6,5", 2500L)>]
+[<InlineData("?###???????? 3,2,1", 506250L)>]
+let ``Puzzle example 2``(src, expected) =
+
+    let src =  Seq.singleton src 
+    let actual = puzzle Case.B src
+    test <@ actual = expected @>           
